5e0fd0f035f30b8bec3762cd1d9cf3d2ea81d013
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2018-11-04 14:17:07 +0100

    Adding storage description

--- BitArray.h
+++ BitArray.h
@@ -1,6 +1,19 @@
 #ifndef BIT_ARRAY
 #define BIT_ARRAY
 
+/* Description of internal, underlying storage used by BitArray
+ *
+ *   wordPositionInArray                             0                                                          1
+ *                       ------------------------------------------------------------------------------------------------------------------
+ *                     ||       9        |       9        |        9       |  5  ||       9        |       9        |        9       |  5  ||
+ *                       ------------------------------------------------------------------------------------------------------------------
+ *                       MSB                                                  LSB  MSB                                                  LSB
+ *   entryOffsetInWord          0                1                 2                      0                1                 2
+ *   position                   1                2                 3                      4                5                 6
+ *                                                                         padding                                                   padding
+ *
+ */
+
 #include "BitArray_utils.h"
 #include "BitArray_static.h"
 #include "BitArray_dynamic.h"
