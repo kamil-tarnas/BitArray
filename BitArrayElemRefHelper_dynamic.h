e944e3754a5327f8d58524cc16d8955e02fbef2e
cavemanjaw_ubuntu <cavemanjaw@op.pl>
2020-01-21 18:55:23 +0100

    More code added to element helper of _dynamic BitArray

--- BitArrayElemRefHelper_dynamic.h
+++ BitArrayElemRefHelper_dynamic.h
@@ -1,6 +1,7 @@
 #ifndef BIT_ARRAY_ELEM_REF_HEPLER_DYNAMIC
 #define BIT_ARRAY_ELEM_REF_HEPLER_DYNAMIC
 
+//No namespace management in this project realy
 namespace dynamic
 {
 	// Does this need to be a template? What would be the use cases of template instances?
@@ -11,12 +12,21 @@ namespace dynamic
 		// Constructors? Befriended with BitArray?
 
 		operator unsigned();
+
+		//Copied from termporary solution in _static, should be private
 		BitArrayElemRefHelper& operator=(unsigned assignment);
 
 	private:
 		unsigned elementOffset;
 		unsigned& refWord;
 	};
+
+	//Can be defined inside class definition
+	BitArrayElemRefHelper::operator=(unsigned assignment)
+	{
+		//How to get the information about sizeOfElement and sizeOfArray?
+		//Can this be get from the _dynamic instantiation of BitArray
+	}
 }
 
 #endif //#ifndef BIT_ARRAY_ELEM_REF_HEPLER_DYNAMIC
